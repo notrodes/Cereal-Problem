4e831fe61ac02d71b2f67cf64fa90434ade28209
Notrodes <reallystupidid@gmail.com>
2019-08-31 17:20:33 -0400

    Improved error handling and UI

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,49 @@
-use std::{io, time};
+use std::{io::stdin, time::{Duration, SystemTime}};
+
 use rand::{Rng, thread_rng};
 
-fn main() {
-    println!("How many times to loop?");
-    let mut user_input = String::new();
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Could not read input.");
-    let user_input = user_input.trim().parse::<u128>().unwrap();
+fn simulation(number_of_loops: usize) -> (usize, usize, f64, Duration) {
     let mut probability = Vec::new();
-    let start = time::SystemTime::now();
-    for _i in 0..user_input {
-        let mut prizes = [0, 0, 0, 0, 0, 0];
-        let mut opens:usize = 0;
+    // Start timer
+    let start = SystemTime::now();
+    // Simulation loop
+    for _i in 0..number_of_loops {
+        let mut prizes: [usize; 6] = [0, 0, 0, 0, 0, 0];
+        let mut opens: usize = 0;
+        // loop until owns every prize
         while prizes.contains(&0) {
             prizes[thread_rng().gen_range(0, 6)] += 1;
             opens += 1;
         }
         probability.push(opens);
     }
+    // End timer
     let timer = start.elapsed().unwrap();
-    println!("Max; {} Min; {} \nThe average {}\nTime elapsed; {:?}",
-             probability.iter().max().unwrap(), probability.iter().min().unwrap(),
-             (probability.iter().sum::<usize>()) as f64 / probability.len() as f64,
-             timer);
-    let mut exit = String::new();
-    io::stdin()
-        .read_line(&mut exit)
-        .expect("Could not read input.");
+    (*probability.iter().max().unwrap(), *probability.iter().min().unwrap(), (probability.iter().sum::<usize>()) as f64 / probability.len() as f64, timer)
+}
+
+fn main() {
+    loop {
+        // User input and parsing
+        println!("How many times to loop? or press [q] to quit.");
+        let mut user_input = String::new();
+        stdin()
+            .read_line(&mut user_input)
+            .expect("Could not read input.");
+        if user_input.trim() == "q" {
+            break;
+        }
+        let user_input = user_input.trim().parse::<usize>();
+        match user_input {
+            // Run main code if parse is Ok()
+            Ok(user_input) => {
+                let stats = simulation(user_input);
+                println!("Max; {} Min; {} \nThe average {}\nTime elapsed; {:?}", stats.0, stats.1, stats.2, stats.3)
+            }
+            // If input is not usize
+            Err(_error) => {
+                eprint!("Input must be integer.\n");
+            }
+        }
+    }
 }
